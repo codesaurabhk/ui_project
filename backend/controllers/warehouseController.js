f1cb744ba8d795816adc3254ae24addf8f6d4334
codesaurabhk <saurabhkumar20843@gmail.com>
2025-06-21 16:47:35 +0530

    Update warehouseController.js

--- backend/controllers/warehouseController.js
+++ backend/controllers/warehouseController.js
@@ -1,22 +1,22 @@
-const Warehouse =  require("../models/Warehouse");
+const Warehouse = require("../models/Warehouse");
 
-exports.getAllWarehouses = async (req , res)=>{
+exports.getAllWarehouses = async (req, res) => {
     const warehouses = await Warehouse.find();
     res.json(warehouses);
 };
 
-exports.createWarehouse = async (req , res)=>{
+exports.createWarehouse = async (req, res) => {
     const newWarehouse = new Warehouse(req.body);
     await newWarehouse.save();
     res.status(201).json(newWarehouse);
 };
 
-exports.updateWarehouse = async (req,res) =>{
-    const updated = await Warehouse.findByIdAndUpdate(req.params.id , req.body , {new : true});
+exports.updateWarehouse = async (req, res) => {
+    const updated = await Warehouse.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.json(updated);
 };
 
-exports.deleteWarehouse = async (req,res) =>{
+exports.deleteWarehouse = async (req, res) => {
     await Warehouse.findByIdAndDelete(req.params.id);
-    res.json({message: "Deleted"});
+    res.json({ message: "Deleted" });
 };
